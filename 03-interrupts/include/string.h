2bd93dd02a4b0118ee83d685c3d60f48dac2e2a8
Joseph Havens <havensjg@gmail.com>
2024-05-06 12:42:31 -0400

    [03-interrupts] Create interrupt demo from 02-printf

---
+++ 03-interrupts/include/string.h
@@ -0,0 +1,7 @@
+#pragma once
+
+#include <stddef.h>
+
+void* memmove(void* dstptr, const void* srcptr, size_t size);
+
+size_t strlen(const char *str);
---
+++ 03-interrupts/readme.md
@@ -0,0 +1,3 @@
+Adapted from 02-printf
+
+Implements an interrupt service routine (ISR) for IRQ 0 from the Programmable Interval Timer
