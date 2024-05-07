9d0898327a52b78034f9e4c1f5b0a349542c7492
Joseph Havens <havensjg@gmail.com>
2024-05-06 20:57:40 -0400

    [04-pitmillis] PIT can now be initialized with frequency

--- 04-pitmillis/src/pit.c
+++ 04-pitmillis/src/pit.c
@@ -1,4 +1,5 @@
 #include <interrupt.h>
+#include <io.h>
 #include <pic.h>
 #include <pit.h>
 
@@ -6,7 +7,15 @@
 volatile int pit_occurred = 0;
 
 /* Initialize the Programmable Interrupt Timer (PIT) */
-void pit_init(void) {
+void pit_init(uint32_t freq) {
+    /* set mode 2 on channel 0, lo/hi byte access, binary count */
+    outb(PIT_CMD, PIT_CH0 | PIT_ACC_LOHI | PIT_MODE_2 | PIT_BINARY);
+
+    /* calculate reload value */
+    uint16_t count = 1193182 / freq;
+    outb(PIT0_DATA, count & 0xff);
+    outb(PIT0_DATA, count >> 8);
+
     /* install the handler for the irq (see irq.s) */
     interrupt_install_irq(0, irq0_wrap);
 
